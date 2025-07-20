d7ccfd50925095d9074057a5f4c10017d41182b8
kevin-sembrat <kevinsembrat2@gmail.com>
2025-07-20 18:44:44 -0400

    connected index.html and explore page

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ function openExploreWindow() {
       // window.open('explore-artwork.html', '_blank');
       window.location.href = 'explore-artwork.html';
     }
+
+function openHomeWindow() {
+      // window.open('explore-artwork.html', '_blank');
+      window.location.href = 'index.html';
+    }
+
+
+function getAll
